68a34d9261fab78b9bc3798fb69e5de43f3650ce
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-09-05 14:30:10 -0600

    set upp the get, post and delete routes within the server.js file

---
+++ server.js
@@ -0,0 +1,80 @@
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const allNotes = require("./Develop/db/db.json");
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//set up get routes including wildcard route for all outside paths
+
+app.get("/api/notes", (req, res) => {
+  res.json(allNotes.slice(1));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//make function for a new note
+function makeNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+
+  if (notesArray.length === 0) notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFile(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+//set up post route
+app.post("./api/notes", (req, res) => {
+  const newNote = makeNewNote(req.body, allNotes);
+  res.json(newNote);
+});
+
+//set up function to delete note
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      fs.writeFile(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+    }
+  }
+}
+
+//set up delete route
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, allNotes);
+  res.json(true);
+});
+
+// Setup listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
